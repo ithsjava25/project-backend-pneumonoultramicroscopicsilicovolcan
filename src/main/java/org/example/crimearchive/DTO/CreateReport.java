97e783e77d049effa27973215b86907052cbbf87
dabuek <xsz300@gmail.com>
2026-03-27 16:26:46 +0100

    fix typos

--- src/main/java/org/example/crimearchive/DTO/CreateReport.java
+++ src/main/java/org/example/crimearchive/DTO/CreateReport.java
@@ -3,7 +3,7 @@ package org.example.crimearchive.DTO;
 import jakarta.validation.constraints.NotBlank;
 
 public record CreateReport(
-        @NotBlank(message = "Måste ha en anlednign till anmälan")
+        @NotBlank(message = "Måste ha en anledning till anmälan")
         String event,
         @NotBlank(message = "Måste ha ett namn på anmälaren")
         String name) {
--- src/main/java/org/example/crimearchive/service/ReportService.java
+++ src/main/java/org/example/crimearchive/service/ReportService.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Service
 public class ReportService {
 
-    SimpleRepository simpleRepository;
+    private final SimpleRepository simpleRepository;
 
 
     public ReportService(SimpleRepository simpleRepository) {
