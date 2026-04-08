a6787f1f6c9ca90b7122cda6a0e74974bd810f5f
danne <xsz300@gmail.com>
2026-04-08 13:05:27 +0200

    retention policy change works as intended

--- src/main/java/org/example/crimearchive/validations/ValidCasenumber.java
+++ src/main/java/org/example/crimearchive/validations/ValidCasenumber.java
@@ -1,8 +1,16 @@
 package org.example.crimearchive.validations;
 
 import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 @Constraint(validatedBy = CaseNumberValidator.class)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ValidCasenumber {
     String message() default "K numret måste vara med typen K-YYYY-XXXXXX eller tomt";
+
+    Class<?>[] groups() default{};
+    Class<? extends Payload>[] payload() default {};
 }
