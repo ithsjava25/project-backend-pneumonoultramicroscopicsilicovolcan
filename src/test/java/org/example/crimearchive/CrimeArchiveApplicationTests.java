8cf6a7c9e13bf0db04639b11817c38339b4e3c59
gurkvatten <johanwilhelmkarlsson@gmail.com>
2026-04-09 10:03:29 +0200

    disabled tests

--- src/test/java/org/example/crimearchive/CrimeArchiveApplicationTests.java
+++ src/test/java/org/example/crimearchive/CrimeArchiveApplicationTests.java
@@ -1,5 +1,6 @@
 package org.example.crimearchive;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 class CrimeArchiveApplicationTests {
 
     @Test
+    @Disabled("Aktiveras när integrationstester är uppsatta")
     void contextLoads() {
     }
+}
 
 }
